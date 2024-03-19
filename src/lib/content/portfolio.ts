68527e01618fabebf1dbb17559523fab8459f2b5
tranguixdev <trang.uixdev@gmail.com>
2024-03-20 04:52:19 +0900

    Modify text.

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -7,11 +7,11 @@
 import { StringKeyValueType } from '../types';
 
 export const socialLinks: StringKeyValueType = {
-  facebook: 'https://www.facebook.com/vatsalsinghkv',
-  instagram: 'https://www.instagram.com/vatsalsinghkv',
-  twitter: 'https://twitter.com/vatsalsinghkv',
-  github: 'https://github.com/vatsalsinghkv',
-  linkedin: 'https://www.linkedin.com/in/vatsalsinghkv/',
+  facebook: 'https://layer3chainfrontend.vercel.app/',
+  instagram: 'https://layer3chainfrontend.vercel.app/',
+  twitter: 'https://layer3chainfrontend.vercel.app/',
+  github: 'https://layer3chainfrontend.vercel.app/',
+  linkedin: 'https://layer3chainfrontend.vercel.app/',
 };
 
 export const author = {
@@ -20,20 +20,10 @@ export const author = {
 };
 
 export const seoData = {
-  title: 'Vatsal Singh | Front-End Developer',
-  description:
-    'Vatsal Singh is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  title: 'Chain title',
+  description: 'Chain title',
   author: author.name,
-  image:
-    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
-  url: 'https://vatsalsinghkv.vercel.app/',
-  keywords: [
-    'Vatsal',
-    'Vatsal Singh',
-    '@vatsalsinghkv',
-    'vatsalsinghkv',
-    'Portfolio',
-    'Vatsal Portfolio ',
-    'Vatsal Singh Portfolio',
-  ],
+  image: '',
+  url: 'https://layer3chainfrontend.vercel.app/',
+  keywords: ['chain'],
 };
