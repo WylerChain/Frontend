3e16060c5b09ef49e0a853743c87b26eea28f33c
tranguixdev <trang.uixdev@gmail.com>
2024-04-10 04:17:36 +0900

    Remove unecessary files.

--- src/app/auth/reset-password/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { ResetPasswordReqForm } from "@/features/auth/components/ResetPasswordReqForm/index.container";
-
-const AuthResetPasswordPage = () => {
-  return <ResetPasswordReqForm />;
-};
-export default AuthResetPasswordPage;
